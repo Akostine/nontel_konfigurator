3e24a2b39fc77f02654494640cccdf2109b5aac5
Akostine <mvagnerok@gmail.com>
2025-08-19 00:13:56 +0200

    Fix useRef import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CustomerHeader from './components/CustomerHeader';
 import MondayStatus from './components/MondayStatus';
